3d43ffc901a3df4f5158702c149fbaa3fcd08df9
Sergei Marochkin <xziggix@gmail.com>
2016-05-24 18:21:03 +0300

    Fix

--- bootstrap.js
+++ bootstrap.js
@@ -20,27 +20,24 @@ define(function(require) {
 	globals.set('containerElement', container);
 
 	// load and insert html
-	require(['core/loader'], function(loader) {
-		// control part
-		require(
-			[
-				'text!template/control.html',
-				'text!core/toolbar/toolbar.html',
-				'text!template/block.html',
-				'text!saved.html',
-			],
-			function(controlHtml, toolbarHtml, blockHtml, savedHtml) {
-				container.insertAdjacentHTML('beforeend', controlHtml);
-				globals.set('controlElement', document.querySelector('div.control'));
+	require(
+		[
+			'text!template/control.html',
+			'text!core/toolbar/toolbar.html',
+			'text!template/block.html',
+			'text!saved.html',
+		],
+		function(controlHtml, toolbarHtml, blockHtml, savedHtml) {
+			container.insertAdjacentHTML('beforeend', controlHtml);
+			globals.set('controlElement', document.querySelector('div.control'));
 
-				container.insertAdjacentHTML('beforeend', blockHtml);
-				globals.set('blockElement', document.querySelector('div.block'));
+			container.insertAdjacentHTML('beforeend', blockHtml);
+			globals.set('blockElement', document.querySelector('div.block'));
 
-				globals.get('controlElement').insertAdjacentHTML('beforeend', toolbarHtml);
-				globals.get('blockElement').insertAdjacentHTML('beforeend', savedHtml);
+			globals.get('controlElement').insertAdjacentHTML('beforeend', toolbarHtml);
+			globals.get('blockElement').insertAdjacentHTML('beforeend', savedHtml);
 
-				require(['core/core']);
-			}
-		);
-	});
+			require(['core/core']);
+		}
+	);
 });
